6f16357c19351b334764bc5a015f53623f9cd253
Thiago Gualberto <thiago.gou@gmail.com>
2022-04-02 15:05:57 -0300

    Application of S.L.D. of the S.O.L.I.D.

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,38 @@
+import { Category } from "../model/Category";
+import {
+    ICategoriesRepository,
+    ICreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+// Objeto responsável por fazer a transferência de dados entre uma classe e outra.
+class CategoriesRepository implements ICategoriesRepository {
+    private categories: Category[];
+
+    constructor() {
+        this.categories = [];
+    }
+
+    create({ name, description }: ICreateCategoryDTO): void {
+        const category = new Category();
+
+        // Função do JS que recebe um objeto e o json que seria seu conteúdo.
+        Object.assign(category, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+
+        this.categories.push(category);
+    }
+
+    list(): Category[] {
+        return this.categories;
+    }
+
+    findByName(name: string): Category {
+        const category = this.categories.find((category) => category.name === name);
+        return category;
+    }
+}
+
+export { CategoriesRepository };
