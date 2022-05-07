231e286bcc6efbd4447b68acffa64a10d08f4963
Thiago Gualberto <thiago.gou@gmail.com>
2022-05-07 17:56:28 -0300

    Applying the stream concept to file uploads.

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,9 +1,16 @@
+import { Request, Response } from "express";
+
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
     constructor(private importCategoryUseCase: ImportCategoryUseCase) { }
     handle(request: Request, response: Response): Response {
         const { file } = request;
+
+        if (!file) {
+            return response.status(400).send("File not sent");
+        }
+
         this.importCategoryUseCase.execute(file);
 
         return response.send();
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,21 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    // Recebe o arquivo vindo do controller.
+    execute(file: Express.Multer.File): void {
+        // Cria um stream de leitura do arquivo.
+        const stream = fs.createReadStream(file.path);
+
+        // Responsável por ler linha por linha do arquivo através do console.log abaixo
+        const parseFile = csvParse();
+
+        // Passa pedaços do arquivo para o 'parseFile'
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async line => {
+            console.log(line);
+        });
     }
 }
 
