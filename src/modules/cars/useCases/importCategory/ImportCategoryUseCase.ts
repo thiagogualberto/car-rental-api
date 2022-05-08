e9346e37418d22a6294d55fe45a32bd3df65848e
Thiago Gualberto <thiago.gou@gmail.com>
2022-05-08 08:21:33 -0300

    Inserting the upload data into the repository.

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -44,7 +44,19 @@ class ImportCategoryUseCase {
     // Recebe o arquivo vindo do controller.
     async execute(file: Express.Multer.File): Promise<void> {
         const categories = await this.loadCategories(file);
-        console.log(categories);
+
+        categories.map(async category => {
+            const { name, description } = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if (!existCategory) {
+                this.categoriesRepository.create({
+                    name,
+                    description,
+                });
+            }
+        });
     }
 }
 
